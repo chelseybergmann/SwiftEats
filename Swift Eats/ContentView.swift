f22b7abb27e5d9db6cc4e56f40e5baeaec626ef6
Chelsey Bergmann <chelseybergmann@email.arizona.edu>
2020-12-05 02:02:26 -0600

    Initial Commit

---
+++ Swift Eats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Eats
+//
+//  Created by Chelsey Bergmann on 12/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
