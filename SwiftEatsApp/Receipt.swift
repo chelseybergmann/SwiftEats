6d93dc6fd13238438a9f060fc593a6a91e0a9b78
cdillingham <51520844+cdillingham@users.noreply.github.com>
2020-12-07 23:32:54 +0200

    Update Receipt.swift

--- SwiftEatsApp/Receipt.swift
+++ SwiftEatsApp/Receipt.swift
@@ -14,11 +14,19 @@ class Receipt: UIViewController {
     @IBOutlet weak var restaurantLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     
+    var entreeString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        func getEntree(entreeString: String) {
+            entreeLabel?.text = entreeString
+        }
+        //override func changeRestLabel( _)
         // Change the label text for each IBOutlet.
         // Ex entreeLabel?.text = "hamburger" but whichever was selected.
-        
+        totalLabel?.text = "$5.68"
+        //entreeLabel?.text = getEntree(entreeString: <#T##String#>)
     }
 }
